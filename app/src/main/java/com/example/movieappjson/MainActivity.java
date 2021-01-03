c69123597e1fcf03cebad64986fa8d87ce9ab023
Mngatia <nyawirahngatia@gmail.com>
2021-01-03 21:02:43 +0300

    Movie app parsing JSON from URL into RecyclerView.

--- app/src/main/java/com/example/movieappjson/MainActivity.java
+++ app/src/main/java/com/example/movieappjson/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     //1. JSON link on Internet https://run.mocky.io/v3/ee97f344-d177-489b-8630-08ede9439b41
     private static String JSON_URL = "https://run.mocky.io/v3/ee97f344-d177-489b-8630-08ede9439b41";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
